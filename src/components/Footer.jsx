9a95c0f9ee66cc35cc0b45f8854f4d84c45d319f
Dhruvvv Joshi <joshidhruv5657@gmail.com>
2024-10-23 16:38:30 +0530

    Personal Portfolio

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Footer() {
+  return (
+    <div class="text-center p-6 bg-black footer">
+      <h1 class="text-white">Get in Touch</h1>
+      <p class="text-white">Email: joshidhruv5657@gmail.com</p>
+      <p class="text-white">Phone: +91 9313748136</p>
+    </div>
+  )
+}
+
+
+export default Footer
